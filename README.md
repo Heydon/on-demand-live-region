584a95e4f34db5318297ca4e1d6dbc17620651b9
Heydon Pickering <heydon@heydonworks.com>
2017-08-22 18:43:24 +0100

    examples and README

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# on-demand-live-regions
+# On Demand Live Regions
+
+A tiny module for making screen readers announce text on demand, without a visual change to the interface. Initializing and using the live region is as simple as:
+
+```js
+const liveRegion = new OnDemandLiveRegion()
+
+liveRegion.say('Hello World!')
+```
+
+## Settings object
+
+* `level` — the live region level (default: `polite`)
+* `parent` — the element to which the live region will be append (default: `body`)
+* `idPrefix` — the prefix for the unique `id` generated for the live region (default: `live-region-`)
+* `delay` — the delay in milliseconds before the phrase is announced (default: `0`)
+
+## Assertive example with a half second delay
+
+```js
+const liveRegionDelayed = new OnDemandLiveRegion({
+  level: 'assertive',
+  delay: 500
+})
+
+liveRegionDelayed.say('Hello World! (sorry, delayed reaction)')
+```
+
+You can override the delay setting in a second argument when calling `say`:
+
+```js
+liveRegionDelayed.say('Hello World! (sorry, delayed reaction)', 1000)
+```
+
+## Say it over and over
+
+Because each time you use the `say` method any previous live region is replaced, you can reliably make the same announcement multiple times. This is not guaranteed in other implementations, where the same live region is repopulated.
---
+++ on-demand-live-region.js
@@ -0,0 +1,71 @@
+/* global define */
+
+(function (global) {
+  'use strict'
+
+  // Constructor
+  function OnDemandLiveRegion (options) {
+    options = options || {}
+
+    // The default settings for the module.
+    this.settings = {
+      level: 'polite',
+      parent: 'body',
+      idPrefix: 'live-region-',
+      delay: 0
+    }
+
+    // Overwrite defaults where they are provided in options
+    for (var setting in options) {
+      if (options.hasOwnProperty(setting)) {
+        this.settings[setting] = options[setting]
+      }
+    }
+
+    // Cast parent as DOM node
+    this.settings.parent = document.querySelector(this.settings.parent)
+
+  }
+
+  // 'Say' method
+  OnDemandLiveRegion.prototype.say = function (thingToSay, delay) {
+    // Get rid of old live region if it exists
+    var oldRegion = this.settings.parent.querySelector('[id^="live-region-"]') || false
+    if (oldRegion) {
+      this.settings.parent.removeChild(oldRegion)
+    }
+
+    // Did an override level get set?
+    var delay = delay|| this.settings.delay
+
+    // Create fresh live region
+    this.currentRegion = document.createElement('span')
+    this.currentRegion.id = this.settings.idPrefix + Math.floor(Math.random() * 10000)
+    this.currentRegion.setAttribute('aria-live', this.settings.level)
+
+    // Hide live region element, but not from assistive technologies
+    this.currentRegion.setAttribute('style', 'clip: rect(1px, 1px, 1px, 1px); height: 1px; overflow: hidden; position: absolute; white-space: nowrap; width: 1px')
+
+    // Add live region to its designated parent
+    this.settings.parent.appendChild(this.currentRegion)
+
+    // Populate live region to trigger it
+    window.setTimeout(function () {
+      this.currentRegion.textContent = thingToSay
+    }.bind(this), delay)
+
+    return this
+  }
+
+  // Export OnDemandLiveRegion
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = OnDemandLiveRegion
+  } else if (typeof define === 'function' && define.amd) {
+    define('OnDemandLiveRegion', [], function () {
+      return OnDemandLiveRegion
+    })
+  } else if (typeof global === 'object') {
+    // attach to window
+    global.OnDemandLiveRegion = OnDemandLiveRegion
+  }
+}(this))
